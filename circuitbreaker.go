0ba71f424cf4b8f573b22314ccf6923fa4ba0141
sndb <sndb@sndb.xyz>
2021-09-02 21:53:55 +0300

    init

---
+++ circuitbreaker.go
@@ -0,0 +1,40 @@
+package patterns
+
+import (
+	"context"
+	"errors"
+	"sync"
+	"time"
+)
+
+type Circuit func(context.Context) (string, error)
+
+func Breaker(c Circuit, threshold int) Circuit {
+	remaining := threshold
+	var last time.Time
+	var mu sync.RWMutex
+
+	return func(ctx context.Context) (string, error) {
+		mu.RLock()
+		if remaining <= 0 {
+			if time.Now().Before(last.Add(time.Second * 2 << (-remaining))) {
+				mu.RUnlock()
+				return "", errors.New("open circuit")
+			}
+		}
+		mu.RUnlock()
+
+		r, err := c(ctx)
+
+		mu.Lock()
+		defer mu.Unlock()
+
+		last = time.Now()
+		if err != nil {
+			remaining--
+		} else {
+			remaining = threshold
+		}
+		return r, err
+	}
+}
---
+++ circuitbreaker_test.go
@@ -0,0 +1,141 @@
+package patterns
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"math/rand"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestCircuitBreaker(t *testing.T) {
+	var timer *time.Timer
+	var total, i int
+	var mu sync.Mutex
+	e := Breaker(func(ctx context.Context) (string, error) {
+		mu.Lock()
+		defer mu.Unlock()
+
+		total++
+		if i >= 3 {
+			if timer != nil {
+				timer.Stop()
+			}
+			timer = time.AfterFunc(time.Second*1, func() {
+				mu.Lock()
+				defer mu.Unlock()
+				i = 0
+			})
+			return "", errors.New("please wait")
+		}
+		i++
+		return "success", nil
+	}, 3)
+
+	for i := 0; i < 100; i++ {
+		e(context.Background())
+	}
+
+	time.Sleep(time.Second * 3)
+
+	if total != 6 {
+		t.Errorf("total == %d, want %d", total, 6)
+	}
+	e(context.Background())
+	if total != 7 {
+		t.Errorf("total == %d, want %d", total, 7)
+	}
+}
+
+func failAfter(threshold int) Circuit {
+	count := 0
+
+	return func(ctx context.Context) (string, error) {
+		count++
+		if count > threshold {
+			return "", errors.New("intentional fail!")
+		}
+		return "success", nil
+	}
+}
+
+func waitAndContinue() Circuit {
+	return func(ctx context.Context) (string, error) {
+		time.Sleep(time.Second)
+		if rand.Int()%2 == 0 {
+			return "success", nil
+		}
+		return "", fmt.Errorf("forced failure")
+	}
+}
+
+func TestCircuitBreakerWithFailAfter(t *testing.T) {
+	circuit := failAfter(5)
+	breaker := Breaker(circuit, 1)
+
+	circuitOpen := false
+	doesCircuitOpen := false
+	doesCircuitReclose := false
+
+	count := 0
+	for range time.NewTicker(time.Second).C {
+		_, err := breaker(context.Background())
+
+		if err != nil {
+			if err.Error() == "open circuit" {
+				if !circuitOpen {
+					circuitOpen = true
+					doesCircuitOpen = true
+
+					t.Log("circuit has opened")
+				}
+			} else {
+				if circuitOpen {
+					circuitOpen = false
+					doesCircuitReclose = true
+
+					t.Log("circuit has automatically closed")
+				}
+			}
+		} else {
+			t.Log("circuit closed and operational")
+		}
+
+		count++
+		if count >= 10 {
+			break
+		}
+	}
+
+	if !doesCircuitOpen {
+		t.Error("circuit didn't appear to open")
+	}
+	if !doesCircuitReclose {
+		t.Error("circuit didn't appear to close after time")
+	}
+}
+
+func TestCircuitBreakerDataRace(t *testing.T) {
+	ctx := context.Background()
+
+	circuit := waitAndContinue()
+	breaker := Breaker(circuit, 1)
+
+	var wg sync.WaitGroup
+	for count := 1; count <= 20; count++ {
+		wg.Add(1)
+
+		go func(count int) {
+			defer wg.Done()
+
+			time.Sleep(50 * time.Millisecond)
+
+			_, err := breaker(ctx)
+
+			t.Logf("attempt %d: err=%v", count, err)
+		}(count)
+	}
+	wg.Wait()
+}
